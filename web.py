17acc6aa592c10fcce553d633ab35f5f1d3b0225
Laura Rokita <laura.rokita@gmail.com>
2015-04-21 13:44:18 -0500

    css is up

--- web.py
+++ web.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.scss import Scss
 
 app = Flask(__name__)
+app.debug = True
+Scss(app, static_dir='static', asset_dir='assets')
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()

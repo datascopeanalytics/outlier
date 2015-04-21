310d38d63929e5522d0d9fe93814b7c4c28180ac
Laura Rokita <laura.rokita@gmail.com>
2015-04-21 15:36:50 -0500

    ordering faux controls

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -12,7 +12,7 @@ var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top");
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#barchart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")

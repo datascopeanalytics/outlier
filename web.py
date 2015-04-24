8c49471b9107282263b89e852d6ac573466c0e2b
Laura Rokita <laura.rokita@gmail.com>
2015-04-24 14:47:25 -0500

    sort chosen options

--- web.py
+++ web.py
@@ -12,13 +12,26 @@ Scss(app, static_dir='static', asset_dir='assets')
 @app.route('/')
 def hello_world():
     raw_data = data.get_survey_responses()
+    chosen_options = get_chosen_options(raw_data)
+
+    return render_template('index.html', raw_data=raw_data, chosen_options=chosen_options)
+
+def get_chosen_options(raw_data):
+    #get unique options with sets
     chosen_options = {"grade": set(), "district": set()};
     for response in raw_data:
         chosen_options["grade"].add(response["grade"])
         chosen_options["district"].add(response["district"])
 
-    return render_template('index.html', raw_data=raw_data, chosen_options=chosen_options)
+    # sort grades
+    chosen_options["grade"] = sorted(list(chosen_options["grade"]))
+    if('k' in chosen_options["grade"]):
+        chosen_options["grade"].remove('k')
+        chosen_options["grade"].insert(0,'k')
 
+    # sort districts
+    chosen_options["district"] = sorted(list(chosen_options["district"]))
+    return chosen_options
 
 if __name__ == '__main__':
     app.run()
